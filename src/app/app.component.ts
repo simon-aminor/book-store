d5ec1e4e5db3600ab626a35f1900926446f78783
Saman <samanaminoroaya@gmail.com>
2024-11-18 15:52:21 +0330

    project started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppheaderComponent } from './components/container/header/header.component';
+import { AppfooterComponent } from './components/container/footer/footer.component';
+import { AppmainComponent } from './components/container/main-div/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppheaderComponent, AppmainComponent, AppfooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'book-store';
